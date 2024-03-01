768d7c6d4fb911e20f02fbdfd522827bdce1eb14
Jbtzcodes <laminutedecode@gmail.com>
2024-03-01 13:45:12 +0100

    mise a jour

--- E-Event-Loop/01.Thread-pool/index.js
+++ E-Event-Loop/01.Thread-pool/index.js
@@ -1,15 +1,10 @@
-// Un "Thread Pool" est une technique de gestion des threads utilisée dans de nombreux langages de programmation, y compris Node.js, pour améliorer les performances lors de l'exécution de tâches concurrentes. Les threads sont des flux d'exécution séparés au sein d'un programme qui peuvent fonctionner en parallèle les uns avec les autres.
 
-
-
-// Le module crypto de Node.js fournit une interface pour effectuer des opérations de cryptographie, telles que la génération de hachages, le chiffrement et le déchiffrement. Lorsque vous utilisez le module crypto pour effectuer des opérations intensives, Node.js peut parfois les exécuter dans le "Thread Pool" pour éviter de bloquer le thread principal.
 
 
 const crypto = require('crypto');
 
-// Chaîne de caractères à hacher
 
-// Le hachage (hashing) est une technique utilisée en informatique pour convertir des données de taille variable en une valeur de taille fixe, généralement sous forme de chaîne de caractères hexadécimale. Cette valeur résultante est appelée "hachage" ou "hash".
+
 const dataToHash = 'Hello, World!';
 
 // Fonction pour hacher les données de manière asynchrone
@@ -24,17 +19,6 @@ function hashDataAsync(data, callback) {
     });
 }
 
-// data: C'est la donnée d'entrée à partir de laquelle la clé dérivée sera calculée. Il peut s'agir d'une chaîne de caractères, d'un Buffer ou d'un tableau d'octets. Dans cet exemple, il semble que cette valeur soit fournie directement dans la variable data.
-
-// '': C'est le mot de passe. Dans ce cas, le mot de passe est vide (''). Cela signifie que la clé sera dérivée uniquement à partir de la donnée d'entrée (data) sans l'ajout d'un mot de passe supplémentaire. Si vous aviez un mot de passe, il serait fourni ici.
-
-// 100000: C'est le nombre d'itérations. Plus ce nombre est élevé, plus le processus de dérivation sera long et sécurisé. Dans cet exemple, 100 000 itérations sont utilisées.
-
-// 64: C'est la longueur de la clé dérivée en octets. Dans cet exemple, la clé dérivée aura une longueur de 64 octets.
-
-// 'sha256': C'est l'algorithme de hachage utilisé. Dans cet exemple, l'algorithme SHA-256 est utilisé. C'est un algorithme de hachage cryptographique réputé pour sa sécurité.
-
-// Appel de la fonction pour hacher les données de manière asynchrone
 hashDataAsync(dataToHash, (err, hashedData) => {
     if (err) {
         console.error('Erreur lors du hachage :', err);
